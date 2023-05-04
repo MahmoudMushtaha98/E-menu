e649479532cbc4301f532167477553bb7c761a07
MahmoudMushtaha98 <118838298+MahmoudMushtaha98@users.noreply.github.com>
2023-05-04 04:32:29 +0300

    Update add_photo.dart

--- screen/add_photo.dart
+++ screen/add_photo.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:newemenu/widget/textbutton.dart';
 
 import '../widget/meal_widget.dart';
 
@@ -8,13 +10,17 @@ class AddPhoto extends StatelessWidget {
   final Map categoriesWithMeal;
   final List categories;
   final List numberOfMeals;
+  final _firestore=FirebaseFirestore.instance;
+  final String id;
 
-  const AddPhoto({Key? key, required this.categoriesWithMeal, required this.categories, required this.numberOfMeals}) : super(key: key);
+   AddPhoto({Key? key, required this.categoriesWithMeal, required this.categories, required this.numberOfMeals, required this.id}) : super(key: key);
 
 
 
   @override
   Widget build(BuildContext context) {
+    double sizeHeight = MediaQuery.of(context).size.height;
+    double sizeWidth = MediaQuery.of(context).size.width;
     List<String> mealNames = [];
     List<double> mealPrice = [];
     List<String> mealComponents = [];
@@ -41,35 +47,65 @@ class AddPhoto extends StatelessWidget {
     }
 
     return Scaffold(
+
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
-            children:List.generate(categories.length, (counter) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(categories[counter],style: Theme.of(context).textTheme.titleLarge,),
+            children: [
+              Column(
+                children:List.generate(categories.length, (counter) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(categories[counter],style: TextStyle(fontSize: sizeWidth*0.07,color: Colors.blueGrey),),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey,width: 2),
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: List.generate(numberOfMeals[counter], (index) {
+                                return MealWidget(mealName: mealNames[index+display(counter)], mealComponent: mealComponents[index+display(counter)], price: mealPrice[index+display(counter)].toString());
+                              }),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: List.generate(numberOfMeals[counter], (index) {
-                          return MealWidget(mealName: mealNames[index+display(counter)], mealComponent: mealComponents[index+display(counter)], price: mealPrice[index+display(counter)].toString());
-                        }),
-                      ),
-                    )
-                  ],
-                ),
-              );
-            })
-            ,
+                  );
+                })
+                ,
+              ),
+              TextButtonWidget(text: 'try', function: () {
+                int j=0;
+                for(int counter=0;counter<categories.length;counter++){
+                  String cat=categories[counter];
+                  for(int i=0;i<numberOfMeals[counter];i++){
+                    _firestore.collection('All').add({
+                      'counter': i,
+                      'emailID':id,
+                      'mealComponents':mealComponents[j],
+                      'mealName':mealNames[j],
+                      'price':mealPrice[j],
+                      'type':cat
+                    });
+                   j++;
+                  }
+                }
+              },)
+            ],
           ),
         ),
       ),
     );
   }
 }
+// print('$i$cat : name :${mealNames[j]}, component: ${mealComponents[j]}, price: ${mealPrice[j]}');
