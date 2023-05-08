e0219a3395cd8bc01346b088c71f82c8cbe340bf
MahmoudMushtaha98 <118838298+MahmoudMushtaha98@users.noreply.github.com>
2023-05-08 16:24:09 +0300

    Update menu_screen.dart

--- screen/menu_screen.dart
+++ screen/menu_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:newemenu/model/save_order_model.dart';
+import 'package:newemenu/screen/add_to_basket.dart';
 import 'package:newemenu/widget/meal_widget.dart';
 
 import '../model/meal_model.dart';
@@ -15,34 +18,122 @@ class MeanuScreen extends StatefulWidget {
 }
 
 class _MeanuScreenState extends State<MeanuScreen> {
+  List<SaveOrderModel> _saveOrder=[];
+  void _saveOrderr(SaveOrderModel saveOrderModel){
+    setState(() {
+      _saveOrder.add(saveOrderModel);
+      print(_saveOrder.length);
+    });
+  }
+
+  Map<String,List<MealModel>> display={};
+  void reorder(){
+    for(int counter=0;counter<widget.catigories.length;counter++){
+      String catigory=widget.catigories[counter];
+      display[catigory] = [];
+      for(int counterr=0;counterr<widget.meals.length;counterr++){
+        if(catigory == widget.meals[counterr].type){
+          display[catigory]!.add(widget.meals[counterr]);
+        }
+      }
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays ([]);
+  }
+
 
   @override
   Widget build(BuildContext context) {
     double sizeHeight = MediaQuery.of(context).size.height;
     double sizeWidth = MediaQuery.of(context).size.width;
+    reorder();
     return Scaffold(
+      floatingActionButton: _saveOrder.length != 0
+          ? Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Stack(
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddToBasket(saveOrder: _saveOrder,)),
+                );
+              },
+              backgroundColor: Color.fromRGBO(212, 175, 55, 1),
+              child: Icon(Icons.add_shopping_cart),
+            ),
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                alignment: Alignment.center,
+                width: 20,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: Text('${_saveOrder.length}'),
+              ),
+            ),
+          ],
+        ),
+      )
+          : null,
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(
-          children: List.generate(widget.catigories.length, (index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(widget.catigories[index],style:  TextStyle(fontSize: sizeWidth*0.07,color: Colors.blueGrey),),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey,width: 2),
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                  ),
-                  child: Column(
-                    children: List.generate(widget.meals.length, (index) {
-                      return MealWidget(mealName: widget.meals[index].mealName, mealComponent: widget.meals[index].mealComponents, price:widget.meals[index].price.toString() );
-                    }),
-                  ),
-                )
-              ],
-            );
-          }),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                children: List.generate(widget.catigories.length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(widget.catigories[index],style:  TextStyle(fontSize: sizeWidth*0.07,color: Colors.blueGrey),),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey,width: 2),
+                            borderRadius: const BorderRadius.all(Radius.circular(15)),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: List.generate(display[widget.catigories[index]]!.length, (mealIndex) {
+                                return Column(
+                                  children: [
+                                    MealWidget(
+                                      mealName: display[widget.catigories[index]]![mealIndex].mealName,
+                                      mealComponent: display[widget.catigories[index]]![mealIndex].mealComponents,
+                                      price: display[widget.catigories[index]]![mealIndex].price.toString(),
+                                      saveOrder: _saveOrderr,
+                                    ),
+
+                                    const SizedBox(height: 8,)
+                                  ],
+                                );
+                              }),
+                            ),
+                          ),
+
+                        )
+                      ],
+                    ),
+                  );
+                }),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
