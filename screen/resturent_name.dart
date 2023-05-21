9993f6803820a4cb82ae6c1be2adc3084cc05cf5
MahmoudMushtaha98 <118838298+MahmoudMushtaha98@users.noreply.github.com>
2023-05-21 03:54:28 +0300

    Update resturent_name.dart

--- screen/resturent_name.dart
+++ screen/resturent_name.dart
@@ -1,7 +1,7 @@
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:newemenu/screen/name_of_category.dart';
 import 'package:newemenu/widget/textbutton.dart';
 
@@ -15,7 +15,7 @@ class RestaurantName extends StatefulWidget {
 }
 
 class _RestaurantNameState extends State<RestaurantName> {
-  TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
 
   final _firestore = FirebaseFirestore.instance;
 
@@ -41,7 +41,7 @@ class _RestaurantNameState extends State<RestaurantName> {
       _isAddButtonPressed = false;
     });
   }
-
+bool _saving=false;
 
   @override
   Widget build(BuildContext context) {
@@ -49,91 +49,129 @@ class _RestaurantNameState extends State<RestaurantName> {
     double sizeHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: sizeHeight*0.5,
-              child: Image.asset('images/E.png',fit: BoxFit.cover),
-            ),
-            SizedBox(width: double.infinity,),
-            SizedBox(
-              width: sizeWidth*0.8,
-              height: sizeHeight*0.07,
-              child: TextField(
-                controller: _controller,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  label: Text('Restaurants name',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromRGBO(212, 175, 55, 1),
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                  ),
+      body: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: sizeHeight*0.5,
+                child: Image.asset('images/E.png',fit: BoxFit.cover),
+              ),
+              const SizedBox(width: double.infinity,),
+              SizedBox(
+                width: sizeWidth*0.8,
+                height: sizeHeight*0.07,
+                child: TextField(
+                  controller: _controller,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    label: Text('Restaurants name',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(212, 175, 55, 1),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
 
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: sizeHeight*0.06,),
-            Text('How many categories',style: TextStyle(fontWeight: FontWeight.bold,fontSize: sizeHeight*0.03,color: Colors.grey),),
-            SizedBox(height: sizeHeight*0.06,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      shape: BoxShape.circle
-                  ),
-                  child: IconButton(
-                    onPressed: _numValue > 0 ? _decrement : null,
-                    icon: Icon(Icons.remove,color: Colors.white),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(
-                    "$_numValue",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+              SizedBox(height: sizeHeight*0.06,),
+              Text('How many categories',style: TextStyle(fontWeight: FontWeight.bold,fontSize: sizeHeight*0.03,color: Colors.grey),),
+              SizedBox(height: sizeHeight*0.06,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.grey,
+                        shape: BoxShape.circle
+                    ),
+                    child: IconButton(
+                      onPressed: _numValue > 0 ? _decrement : null,
+                      icon: const Icon(Icons.remove,color: Colors.white),
                     ),
                   ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(212, 175, 55, 1),
-                      shape: BoxShape.circle
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      "$_numValue",
+                      style: const TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  child: IconButton(
-                    onPressed: _increment,
-                    icon: Icon(Icons.add,color: Colors.white),
-                    color: _isAddButtonPressed ? Colors.green : null,
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(212, 175, 55, 1),
+                        shape: BoxShape.circle
+                    ),
+                    child: IconButton(
+                      onPressed: _increment,
+                      icon: const Icon(Icons.add,color: Colors.white),
+                      color: _isAddButtonPressed ? Colors.green : null,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: sizeHeight*0.06,),
-            TextButtonWidget(text: 'Submit', callback: () {
-              try{
-                _firestore.collection('RestaurantName').add({
-                  'emailID': widget.id,
-                  'numberOfCategories': _numValue,
-                  'restaurantName' : _controller.text
+                ],
+              ),
+              SizedBox(height: sizeHeight*0.06,),
+              TextButtonWidget(text: 'Submit', callback: () async{
+                setState(() {
+                  _saving=true;
                 });
-                Navigator.push(context, MaterialPageRoute(builder: (context) => NameOfCategory(numberOfCategory: _numValue, id: widget.id),));
-              }catch(e){
-                print(e);
-              }
-            },)
-          ],
+                await deleteRestaurant();
+                if(mounted) {
+                  await selectName(context);
+                }
+              },)
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> deleteRestaurant() async {
+    try{
+      final del=await _firestore.collection('RestaurantName').where('emailID',isEqualTo: widget.id).get();
+      if(del.docs.isNotEmpty) {
+        for (var dele in del.docs) {
+          dele.reference.delete();
+        }
+      }
+    }catch(e){
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> selectName(BuildContext context) async {
+    try{
+      await _firestore.collection('RestaurantName').add({
+        'emailID': widget.id,
+        'numberOfCategories': _numValue,
+        'restaurantName' : _controller.text
+      });
+      if(mounted) {
+        setState(() {
+          _saving=false;
+        });
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) =>
+              NameOfCategory(
+                  numberOfCategory: _numValue, id: widget.id),));
+      }
+    }catch(e){
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 }
