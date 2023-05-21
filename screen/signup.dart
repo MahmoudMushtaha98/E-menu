cbf648f8a5e4a5d1c945fba02dd5142453b292e5
MahmoudMushtaha98 <118838298+MahmoudMushtaha98@users.noreply.github.com>
2023-05-21 03:54:49 +0300

    Update signup.dart

--- screen/signup.dart
+++ screen/signup.dart
@@ -14,13 +14,20 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey=GlobalKey<FormState>();
+  final _formKey2=GlobalKey<FormState>();
   final TextEditingController _email = TextEditingController();
-
   final TextEditingController _password = TextEditingController();
 
   final _auth = FirebaseAuth.instance;
 
   bool _saving = false;
+
+  Future<void> _submitForm()async{
+    if(_formKey.currentState!.validate()&&_formKey2.currentState!.validate()){
+      await signuoGenerate(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,48 +50,66 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   child: Icon(Icons.login,
                       color: Colors.grey, size: sizeWidth * 0.5),
                 ),
-                SizedBox(
-                  width: sizeWidth * 0.8,
-                  height: sizeHeight * 0.07,
-                  child: TextField(
-                    controller: _email,
-                    keyboardType: TextInputType.emailAddress,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      label: Text(
-                        'Email',
-                        style: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.bold),
+                Form(
+                  key: _formKey,
+                  child: SizedBox(
+                    width: sizeWidth * 0.8,
+                    height: sizeHeight * 0.07,
+                    child: TextFormField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        label: Text(
+                          'Email',
+                          style: TextStyle(
+                              color: Colors.grey, fontWeight: FontWeight.bold),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(212, 175, 55, 1),
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(10))),
                       ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color.fromRGBO(212, 175, 55, 1),
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Please enter yor email';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                 ),
                 SizedBox(
                   height: sizeHeight * 0.04,
                 ),
-                SizedBox(
-                  width: sizeWidth * 0.8,
-                  height: sizeHeight * 0.07,
-                  child: TextField(
-                    controller: _password,
-                    obscureText: true,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      label: Text(
-                        'Password',
-                        style: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.bold),
+                Form(
+                  key: _formKey2,
+                  child: SizedBox(
+                    width: sizeWidth * 0.8,
+                    height: sizeHeight * 0.07,
+                    child: TextFormField(
+                      controller: _password,
+                      obscureText: true,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        label: Text(
+                          'Password',
+                          style: TextStyle(
+                              color: Colors.grey, fontWeight: FontWeight.bold),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(212, 175, 55, 1),
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(10))),
                       ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color.fromRGBO(212, 175, 55, 1),
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return'Please enter yor password';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                 ),
@@ -92,37 +117,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 TextButtonWidget(
                   text: 'Sign Up',
                   callback: () async {
-                    setState(() {
-                      _saving = true;
-                    });
-                    try {
-                      final newUser =
-                          await _auth.createUserWithEmailAndPassword(
-                        email: _email.text,
-                        password: _password.text,
-                      );
-                      if (newUser.user != null) {
-                        final _firestore = FirebaseFirestore.instance;
-                        _firestore.collection('Users').add({
-                          "isMerchant": false,
-                          "email": newUser.user!.email.toString(),
-                        });
-                      }
-                      if(mounted) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  RestaurantName(id: newUser.user!.uid)));
-                      }
-                      setState(() {
-                        _saving = false;
-                      });
-                    } catch (e) {
-                      if (kDebugMode) {
-                        print(e);
-                      }
-                    }
+                    await _submitForm();
                   },
                 ),
               ],
@@ -131,5 +126,39 @@ class _SignUpScreenState extends State<SignUpScreen> {
         ),
       ),
     );
+  }
+
+  Future<void> signuoGenerate(BuildContext context) async {
+    setState(() {
+      _saving = true;
+    });
+    try {
+      final newUser =
+          await _auth.createUserWithEmailAndPassword(
+        email: _email.text,
+        password: _password.text,
+      );
+      if (newUser.user != null) {
+        final _firestore = FirebaseFirestore.instance;
+        _firestore.collection('Users').add({
+          "isMerchant": false,
+          "email": newUser.user!.email.toString(),
+        });
+      }
+      if(mounted) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  RestaurantName(id: newUser.user!.uid)));
+      }
+      setState(() {
+        _saving = false;
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 }
